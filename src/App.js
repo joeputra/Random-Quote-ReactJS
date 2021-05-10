7056a2fd1d1649d6d820f3b788e7992830db46a0
joeputra <joe.putra40@gmail.com>
2021-05-10 13:14:11 +0700

    first commit Text and background

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+import './App.css'
+
+export class App extends Component {
+  state = { advice: '' }
+
+  componentDidMount() {
+    this.fetchAdvice()
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get(`https://api.adviceslip.com/advice`)
+      .then((response) => {
+        const { advice } = response.data.slip
+        this.setState({ advice })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    const { advice } = this.state
+    return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
